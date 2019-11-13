a563f13731ce268b04a5005d7de48c4332c1754f
fstj11@gmail.com <fstj11@gmail.com>
2019-11-12 21:12:30 -0800

    More modifications to parser.hpp

--- src/parser.hpp
+++ src/parser.hpp
@@ -23,31 +23,48 @@ class Parser {
 	    // Parse userInput into tokens using the ;, &&, || separators
 	    for (tokenizer::iterator iter = tokens.begin(); iter != tokens.end(); ++iter) {
 		string parsed = *iter;
+		/* FIXME POTENTIALLY	
 		if (parsed.find("\"") == string::npos) {
 		    parsed = regex_replace(parsed, regex("^ +"), "");
 		    parsed = regex_replace(parsed, regex(" +$"), ""); // Remove trailing whitespace from parsed token
 		}
+		*/
 		commands.push_back(parsed);
 	    }
 	    
 	    // Correctly account for connectors between " "
 	    vector<string> finalCommands;
-	    for (unsigned int i = 0; i < commands.size() - 1; ++i) {
-		string join = commands.at(i);
-		if (commands.at(i).find("\"") != string::npos) {
-		    while (commands.at(i + 1).find("\"") == string::npos) {
-			join += commands.at(i + 1);
+	    if (commands.size() == 1) {
+		finalCommands.push_back(commands.at(0));
+	    }
+	    else {
+		for (unsigned int i = 0; i < commands.size() - 1; ++i) {
+		    string join = commands.at(i);
+		    if (commands.at(i).find("\"") != string::npos && commands.at(i).substr(commands.at(i).find("\"") + 1, commands.at(i).size()).find("\"") == string::npos) {
+			while (commands.at(i + 1).find("\"") == string::npos) {
+			    join += commands.at(i + 1);
+			    ++i;
+			}
 			++i;
+			if (commands.at(i).find("\"") != string::npos) {
+			    join += commands.at(i);
+			}
 		    }
-		    ++i;
-		    join += commands.at(i);
-		}
-		finalCommands.push_back(join);
+		    finalCommands.push_back(join);
+		}	
 	    }
 	    if (commands.at(commands.size() - 1).find("\"") == string::npos) {
 		finalCommands.push_back(commands.at(commands.size() - 1));
 	    }
 	    
+	    // Remove leading and trailing white space from commands that are not between " "
+	    for (unsigned int i = 0; i < finalCommands.size(); ++i) {
+		if (finalCommands.at(i).find("\"") == string::npos) {
+		    finalCommands.at(i) = regex_replace(finalCommands.at(i), regex("^ +"), "");
+		    finalCommands.at(i) = regex_replace(finalCommands.at(i), regex(" +$"), "");
+		}
+	    }
+    
 	    // If there are connectors at the beginning of the command sequence, remove them
 	    while (finalCommands.at(0) == "&" || finalCommands.at(0) == "|" || finalCommands.at(0) == ";") {
 		finalCommands.erase(finalCommands.begin() + 0);
