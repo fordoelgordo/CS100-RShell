078350517ee9ae50ea62f46e90a78fd1a05c13a4
fstj11@gmail.com <fstj11@gmail.com>
2019-11-26 19:40:45 -0800

    Updated testing framework

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -12,122 +12,99 @@ using namespace std;
 TEST(ParserTest, SimpleCommand) {
     string input = "ls -a;";
     Parser* parsed = new Parser();
-    ExecuteGroup* executable = parsed->parse(input);
-    string test = "ls-a";
-    EXPECT_EQ(test, executable->get_command_full());
+    vector<string> v = parsed->parse(input);
+    vector<string> t;
+    t.push_back("ls -a");
+    t.push_back(";");
+    EXPECT_EQ(t, v);
 }
 TEST(ParserTest, SimpleEcho) {
     string input = "echo hello";
     Parser* parsed = new Parser();
-    ExecuteGroup* executable = parsed->parse(input);
-    string test = "echohello";
-    EXPECT_EQ(test, executable->get_command_full());
+    vector<string> v = parsed->parse(input);
+    vector<string> t;
+    t.push_back("echo hello");
+    EXPECT_EQ(t, v);
 }
 TEST(ParserTest, ChainedCommand) {
     string input = "ls -a; echo hello";
     Parser* parsed = new Parser();   
-    ExecuteGroup* executable = parsed->parse(input);
-    string test = "ls-aechohello";
-    EXPECT_EQ(test, executable->get_command_full());
+    vector<string> v = parsed->parse(input);
+    vector<string> t;
+    t.push_back("ls -a");
+    t.push_back(";");
+    t.push_back("echo hello");
+    EXPECT_EQ(t, v);
 }
 TEST(ParserTest, QuoteEcho) {
     string input = "echo \"hello world\"";  
     Parser* parsed = new Parser();   
-    ExecuteGroup* executable = parsed->parse(input);
-    string test = "echohelloworld";
-    EXPECT_EQ(test, executable->get_command_full());
+    vector<string> v = parsed->parse(input);
+    vector<string> t;
+    t.push_back("echo hello world");
+    EXPECT_EQ(t, v);
 }
 TEST(ParserTest, ConnectorEcho) {
     string input = "echo \"Hello && World\""; 
     Parser* parsed = new Parser();   
-    ExecuteGroup* executable = parsed->parse(input);
-    string test = "echoHello&&World";
-    EXPECT_EQ(test, executable->get_command_full());
+    vector<string> v = parsed->parse(input);
+    vector<string> t;
+    t.push_back("echo Hello && World");
+    EXPECT_EQ(t, v);
 }
 
 // Unit test to ensure that arguments will execute
 TEST(ExecuteTest, SimpleComand) {
-    char* input[] = {"ls", "-a", NULL};
-    Execute* execute = new Execute(input, ";");
-    execute->execute();
-    EXPECT_TRUE(execute->get_success());
+    string input = "ls -a";
+    Parser* parsed = new Parser();
+    EXPECT_TRUE(parsed->execute(parsed->parse(input)));
 }
 TEST(ExecuteTest, EchoCommand) {
-    char* input[] = {"echo", "hello", "world", NULL};
-    Execute* execute = new Execute(input, ";");
-    execute->execute();
-    EXPECT_TRUE(execute->get_success());
+    string input = "echo hello world";
+    Parser* parsed = new Parser();
+    EXPECT_TRUE(parsed->execute(parsed->parse(input)));
 }
 TEST(ExecuteTest, BadCommand) {
-    char* input[] = {"cd", "test", NULL};
-    Execute* execute = new Execute(input, ";");
-    execute->execute();
-    EXPECT_FALSE(execute->get_success());
+    string input = "cd test";
+    Parser* parsed = new Parser();
+    EXPECT_FALSE(parsed->execute(parsed->parse(input)));
 }
 
 // Unit test to test the Test class implementation
 TEST(TestExecuteTest, SimpleCommand) {
-    vector<string> command;
-    command.push_back("test");
-    command.push_back("-e");
-    command.push_back("src/");
-    ExecuteCommand* executable = new TestExecute(command, ";");
-    executable->execute();
-    EXPECT_TRUE(executable->get_success());
+    string input = "test -e src/";
+    Parser* parsed = new Parser();
+    EXPECT_TRUE(parsed->execute(parsed->parse(input)));
 }
 TEST(TestExecuteTest, SimpleFail) {
-    vector<string> command;
-    command.push_back("test");
-    command.push_back("nonexistant/file/path");
-    ExecuteCommand* test = new TestExecute(command, ";");
-    test->execute();
-    EXPECT_FALSE(test->get_success());
+    string input = "test nonexistant/file/path";
+    Parser* parsed = new Parser();
+    EXPECT_FALSE(parsed->execute(parsed->parse(input)));
 }
 TEST(TestExecuteTest, SimpleBracket) {
-    vector<string> command;
-    command.push_back("[");
-    command.push_back("-e");
-    command.push_back("src/");
-    command.push_back("]");
-    ExecuteCommand* test = new TestExecute(command, ";");
-    test->execute();
-    EXPECT_TRUE(test->get_success());
+    string input = "[ -e src/ ]";
+    Parser* parsed = new Parser();
+    EXPECT_TRUE(parsed->execute(parsed->parse(input)));
 }
 TEST(TestExecuteTest, fflag) {
-    vector<string> command;
-    command.push_back("test");
-    command.push_back("-f");
-    command.push_back("src/testexecute.hpp");
-    ExecuteCommand* test = new TestExecute(command, ";");
-    test->execute();
-    EXPECT_TRUE(test->get_success());
+    string input = "test -f src/testexecute.hpp";
+    Parser* parsed = new Parser();
+    EXPECT_TRUE(parsed->execute(parsed->parse(input)));
 }
 TEST(TestExecuteTest, dflag) {
-    vector<string> command;
-    command.push_back("test");
-    command.push_back("-d");
-    command.push_back("src/");
-    ExecuteCommand* test = new TestExecute(command, ";");
-    test->execute();
-    EXPECT_TRUE(test->get_success());
+    string input = "test -d src/";
+    Parser* parsed = new Parser();
+    EXPECT_TRUE(parsed->execute(parsed->parse(input)));
 }
 TEST(TestExecuteTest, fflagFail) {
-    vector<string> command;
-    command.push_back("test");
-    command.push_back("-f");
-    command.push_back("~/assignment-assign_jeff_ford/prototype/");
-    ExecuteCommand* test = new TestExecute(command, ";");
-    test->execute();
-    EXPECT_FALSE(test->get_success());
+    string input = "test -f ~/assignment-assign_jeff_ford/prototype/";
+    Parser* parsed = new Parser();
+    EXPECT_FALSE(parsed->execute(parsed->parse(input)));
 }
 TEST(TestExecuteTest, dflagFail) {
-    vector<string> command;
-    command.push_back("test");
-    command.push_back("-d");
-    command.push_back("~/assignment-assign_jeff_ford/src/testexecute.hpp");
-    ExecuteCommand* test = new TestExecute(command, ";");
-    test->execute();
-    EXPECT_FALSE(test->get_success());
+    string input = "test -d ~/assignment-assign_jeff_ford/src/testexecute.hpp";
+    Parser* parsed = new Parser();
+    EXPECT_FALSE(parsed->execute(parsed->parse(input)));
 }
 
 // main() function to execute tests 
